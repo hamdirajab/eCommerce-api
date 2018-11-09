146d457218467eb14762d4fc941092267f18c992
vagrant <vagrant@restfullapi>
2018-11-09 19:01:56 +0000

    Start with Sort the collection

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -25,8 +25,10 @@ trait ApiResponser
  			return $this->successResponse(['data' => $collection] , $code);
 
  		}
-
+ 		
  		$transformer = $collection->first()->transformer;// to know the model and use the transformer
+
+		$collection = $this->sortData($collection);
 
  		$collection = $this->transformData($collection , $transformer);
 
@@ -45,6 +47,18 @@ trait ApiResponser
  	protected function showMessage($message , $code = 200)
  	{
  		return $this->successResponse(['data' => $message] , $code);
+ 	}
+
+ 	protected function sortData(Collection $collection)
+ 	{
+ 		if (request()->has('sort_by')) 
+ 		{
+ 			$attribute = request()->sort_by;
+
+ 			// $collection = $collection->sortBy($attribute);
+ 			$collection = $collection->sortBy->{$attribute};
+ 		}
+ 		return $collection;
  	}
 
  	protected function transformData($data , $transformer)
